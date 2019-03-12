dd0928b09af3a03b65e84b7febb9afffbea1babf
marian0 <marianosantafe@gmail.com>
2019-03-12 17:02:57 -0300

    - Added some fixture data

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,5 +12,19 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         factory(\App\Models\User::class, 10)->create();
+
+        \App\Models\User::create([
+            'name' => 'Mariano Peyregne',
+            'email' => 'marianosantafe@gmail.com',
+            'api_token' => \Illuminate\Support\Str::random(16),
+            'password' => \Illuminate\Support\Facades\Hash::make('password')
+        ]);
+
+        \App\Models\User::create([
+            'name' => 'Admin Sample',
+            'email' => 'admine@fys.com',
+            'api_token' => \Illuminate\Support\Str::random(16),
+            'password' => \Illuminate\Support\Facades\Hash::make('password')
+        ]);
     }
 }
